831c6f06a9171dab5b4ca1225adde40f2fe924c1
miyuki.kano <miyuki.kano0208@gmail.com>
2024-08-31 23:35:21 +0900

    NewServlet.javaを作成

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,7 +1,7 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
+import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -10,22 +10,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// DTOクラスとあわせるからmodels.Taskとなる
 import models.Task;
 import utils.DBUtil;
 
-
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,12 +33,27 @@ public class IndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
-//      List<DTO class> 変数名、("getAllTask", Task.class)ここはDTOクラス名
-        List<Task> tasks = em.createNamedQuery("getAllTask", Task.class).getResultList();
-//      後ろの方のtasksは上の行の変数と同じにしているだけ
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+        em.getTransaction().begin();
+
+        // Taskのインスタンスを生成
+        Task m = new Task();
+
+        // mの各フィールドにデータを代入
+        String content = "hello";
+        m.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
+        m.setCreated_at(currentTime);
+        m.setUpdated_at(currentTime);
+
+        // データベースに保存
+        em.persist(m);
+        em.getTransaction().commit();
+
+        // 自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(m.getId()).toString());
 
         em.close();
-    }
+     }
 
 }
